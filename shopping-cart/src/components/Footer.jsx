673e4250975c601917efb8de3655986b41a94172
YoussefAyachi95 <youssef.ayachi@web.de>
2023-02-17 09:45:35 +0100

    Add new project

---
+++ shopping-cart/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+export function Footer () {
+  // const { filters } = useFilters()
+
+  return (
+    <footer className='footer'>
+      <h4>Interview Exercise ⚛️ － <span>@youssefayachi95</span></h4>
+      <h5>Shopping Cart with useContext & useReducer</h5>
+    </footer>
+  )
+}
